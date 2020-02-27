b2916536a11a8fac8732c6200937333504e7f660
xinxin <1738733078@qq.com>
2020-02-27 14:20:05 +0800

    update

--- Emojis/connect_mysql.py
+++ Emojis/connect_mysql.py
@@ -14,21 +14,24 @@ def readFileJson(filename):
 
 
 def fromJsonToMysql(filename, table="emojis"):
-    emojis = readFileJson(filename);
-    db = mc.connect(host="49.233.169.5", user="yuchuan", password="081849", database="study", charset="utf8mb4")
+    emojis = readFileJson(filename)
+    # 填入自己主机，用户，密码
+    db = mc.connect(host="", user="", password="",
+                    database="", charset="utf8mb4")
     cur = db.cursor()
-    index = 1;
+    index = 1
     for item in emojis:
         cur.execute(
-            "INSERT INTO %s" % table + "(number,code,bighead,bighead_url,mediumhead,mediumhead_url,browser,apple,google,"
-                                       "facebook,windows,twitter,joy,sams,gmail,sb,dcm,kddi,cldr_short_name,andr) "
-                                       "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+            "INSERT INTO %s" % table +
+            "(number,code,bighead,bighead_url,mediumhead,mediumhead_url,browser,apple,google,"
+            "facebook,windows,twitter,joy,sams,gmail,sb,dcm,kddi,cldr_short_name,andr) "
+            "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
             (item["number"], item["Code"], item["bighead"], item["bighead_url"], item["mediumhead"],
              item["mediumhead_url"], item["Browser"], item["Apple"], item["Google"], item["FaceBook"],
              item["Windows"], item["Twitter"], item["Joy"], item["Sama"], item["GMail"], item["SB"],
-             item["DCM"], item["KDDI"], item["CLDR-Short-Name"], json.dumps(item["andr"])));
+             item["DCM"], item["KDDI"], item["CLDR-Short-Name"], json.dumps(item["andr"])))
         print("成功插入第" + str(index) + "行")
-        index += 1;
+        index += 1
     # 提交
     db.commit()
     cur.close()
